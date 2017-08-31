95bb1481bdd4ba435375f11c25aa3a351c49c1bd
katger4 <kajgertz@gmail.com>
2017-08-30 19:09:45 -0700

    parse series in while loop

--- prepEAD.py
+++ prepEAD.py
@@ -27,60 +27,95 @@ def write_EAD_xml(xmlstr, outfilename):
         outfile.write('\n')
         outfile.write(xmlstr[39:])
 
+def fix_agent_source(name):
+    if '@source' in name and name['@source'] != 'lcnaf':
+        if '@rules' in name and name['@rules'] == 'aacr2':
+            name.pop('@source')
+        else:
+            name['@rules'] = 'aacr2'
+            name.pop('@source')
+
+def parse_series(seriesidx):
+    # remove extra dao tags and make sure all scopecontent has a p tag
+    # remove digitization dates (dont show up in AW as different dates)
+    for d in series[seriesidx]['c02']:
+        if 'dao' in d['did']:
+            d['did']['dao'].pop('@actuate')
+            d['did']['dao'].pop('@type')
+            d['did']['dao'].pop('daodesc')
+
+        if 'scopecontent' in d: 
+            if 'p' not in d['scopecontent']:
+                d['scopecontent']['p'] = {}
+                d['scopecontent']['p'] = d['scopecontent']['#text']
+                d['scopecontent'].pop('#text')
+
+        # remove digitization dates for display purposes
+        if 'unitdate' in d['did']:
+            if type(d['did']['unitdate']) != list:
+                year = d['did']['unitdate']['#text'][:4]
+                if is_digi(year) == True:
+                    d['did'].pop('unitdate')
+            else:
+                for idx,date in enumerate(d['did']['unitdate']):
+                    try:
+                        year = date['#text'][:4]
+                        if is_digi(year) == True:
+                            d['did']['unitdate'].pop(idx)
+                    except TypeError:
+                        year = date[:4]
+                        if is_digi(year) == True:
+                            d['did']['unitdate'].pop(idx)
+        if 'origination' in d['did']:
+            if 'persname' in d['did']['origination']:
+                fix_agent_source(d['did']['origination']['persname'])
+            else:
+                fix_agent_source(d['did']['origination']['corpname'])
 ############################################################
 
 # load converted EAD
-path = input("enter the path and name of the xml file converted using the archives west utility (e.g. ./data/converted_ead.xml): ")
-
+# path = input("enter the path and name of the xml file converted using the archives west utility (e.g. ./data/converted_ead.xml): ")
+# repo = input("enter the repository (media or ethno): ")
+# path = './data/wau_uwea_2008012-c.xml'
+path = './data/wau_croctest.xml'
+repo = 'ethno'
 with open(path) as fd:
     doc = xmltodict.parse(fd.read())
 
 # fix agency codes
-doc['ead']['eadheader']['eadid']['@mainagencycode'] = 'waseumc'
-doc['ead']['archdesc']['did']['unitid']['@repositorycode'] = 'waseumc'
+if repo == 'media':
+    doc['ead']['eadheader']['eadid']['@mainagencycode'] = 'waseumc'
+    doc['ead']['archdesc']['did']['unitid']['@repositorycode'] = 'waseumc'
 
-# remove unnecessary extref tag
-doc['ead']['eadheader']['filedesc']['publicationstmt'].pop('extref')
+# remove unnecessary extref tag if there
+if 'extref' in doc['ead']['eadheader']['filedesc']['publicationstmt']:
+    doc['ead']['eadheader']['filedesc']['publicationstmt'].pop('extref')
+
+# fix origination if wrong
+if type(doc['ead']['archdesc']['did']['origination']) != list:
+    name = doc['ead']['archdesc']['did']['origination']['persname']
+    fix_agent_source(name)
+else:
+    for p in doc['ead']['archdesc']['did']['origination']:
+        name = p['persname']
+        fix_agent_source(name)
 
 # locate the series containing archival objects
 series = doc['ead']['archdesc']['dsc']['c01']
 
-title = input("enter the title of the series containing archival objects (e.g. Sound Recordings): ")
+# # indicate how many series of archival objects to parse
+# num_series = int(input("enter the number of series containing archival objects to reformat: "))
+num_series = 2
 
-for idx,i in enumerate(series):
-    if title in i['did']['unittitle']['#text']:
-        seriesidx = idx
+while num_series > 0:
+    title = input("enter the title of a series containing archival objects (e.g. Sound Recordings): ")
 
-# remove extra dao tags and make sure all scopecontent has a p tag
-# remove digitization dates (dont show up in AW as different dates)
-for d in series[seriesidx]['c02']:
-    if 'dao' in d['did']:
-        d['did']['dao'].pop('@actuate')
-        d['did']['dao'].pop('@type')
-        d['did']['dao'].pop('daodesc')
+    for idx,i in enumerate(series):
+        if title in i['did']['unittitle']['#text']:
+            seriesidx = idx
 
-    if 'scopecontent' in d: 
-        if 'p' not in d['scopecontent']:
-            d['scopecontent']['p'] = {}
-            d['scopecontent']['p'] = d['scopecontent']['#text']
-            d['scopecontent'].pop('#text')
-
-    # remove digitization dates for display purposes
-    if 'unitdate' in d['did']:
-        if type(d['did']['unitdate']) != list:
-            year = d['did']['unitdate']['#text'][:4]
-            if is_digi(year) == True:
-                d['did'].pop('unitdate')
-        else:
-            for idx,date in enumerate(d['did']['unitdate']):
-                try:
-                    year = date['#text'][:4]
-                    if is_digi(year) == True:
-                        d['did']['unitdate'].pop(idx)
-                except TypeError:
-                    year = date[:4]
-                    if is_digi(year) == True:
-                        d['did']['unitdate'].pop(idx)
+    parse_series(seriesidx)
+    num_series -= 1
 
 # convert dict to json to xmlstring
 xmlstr = dict2xmlstr(doc, pretty = True)
