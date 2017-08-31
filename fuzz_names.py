a0599219ffd04e167e169b3ea7218c773ee2a3bd
katger4 <kajgertz@gmail.com>
2017-08-30 21:05:23 -0700

    add fuzz strategy for person names

--- fuzz_names.py
+++ fuzz_names.py
@@ -22,28 +22,40 @@ def write_tsv(data, output):
 
 ############################################################
 
-lcnaf = load_csv('./data/lcnaf.tsv')
+lcnaf = load_csv('./data/lcnaf.csv')
 print(len(lcnaf))
 
+agent_type = input('Enter the type of agent records (people or corporate): ')
 # escape_term = input('Enter any phrase to remove from the lc term (e.g. (Musical group): ')
-escape_term = ' (Musical group)'
-escape_term2 = ' (Rock group)'
 
 likely = []
 unlikely = []
-for name,lc in list(lcnaf.items()):
-	name_test = name.replace('The', '')
-	lc_test = lc.replace(escape_term,'').replace(escape_term2,'')
-	#print((name_test, lc_test, fuzz.token_sort_ratio(name_test, lc_test)))
-	ratio = fuzz.token_sort_ratio(name_test, lc_test)
-	if ratio >= 80:
-		likely.append((name,lc))
-	else:
-		unlikely.append((name,lc))
+
+if agent_type == 'corporate':
+	escape_term = ' (Musical group)'
+	escape_term2 = ' (Rock group)'
+	for name,lc in list(lcnaf.items()):
+		name_test = name.replace('The', '')
+		lc_test = lc.replace(escape_term,'').replace(escape_term2,'')
+		#print((name_test, lc_test, fuzz.token_sort_ratio(name_test, lc_test)))
+		ratio = fuzz.token_sort_ratio(name_test, lc_test)
+		if ratio >= 80:
+			likely.append((name,lc))
+		else:
+			unlikely.append((name,lc))
+elif agent_type == 'people':
+	for name,lc in list(lcnaf.items()):
+
+		ratio = fuzz.token_sort_ratio(name, lc)
+		if ratio >= 75:
+			likely.append((name,lc))
+		else:
+			# print((name, lc, fuzz.token_sort_ratio(name, lc)))
+			unlikely.append((name,lc))
 
 print(len(likely))
 print(len(unlikely))
 
-write_tsv(likely, './data/lcnaf_fuzz.tsv')
-write_tsv(unlikely, './data/unlikely.tsv')
+write_tsv(likely, './data/lcnaf_fuzz_p.tsv')
+write_tsv(unlikely, './data/unlikely_p.tsv')
 
